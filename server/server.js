5e6841827a72cb94ddbae95cb4dc6ff164a242ab
Matthew Hackett <195298367+matthew-hackett@users.noreply.github.com>
2026-03-14 12:00:21 +1100

    TODO for server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,16 @@ const server = new WebSocketServer({
   port: 1234 
 });
 
+function compare(signal1, signal2) {
+    return false;
+}
+
+// TODO:
+// - store previous
+// - on new ones, check with previous
+// - purge old ones
+// - if successful, send a successful message to each with the rtc connection info
+
 server.on('connection', (socket) => {
     console.log('Client connected');
 
